a6dc3ba5e3548e8195dc1fe1fcc94ff09f4f78de
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-08-08 15:59:19 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cp -rf strapi/* ./
+sed -i "s~ARG BASE_VERSION~ARG BASE_VERSION=latest~g" Dockerfile
+sed -i "s~ARG STRAPI_VERSION~ARG STRAPI_VERSION=latest~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/strapi:latest | docker load
