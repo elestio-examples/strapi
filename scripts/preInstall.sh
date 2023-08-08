016ee5a3b62bd1163ea26d8aa7901e6222179a29
Prem chavhan <pchavhan@logiquad.com>
2023-08-08 13:36:37 +0530

    Updated Fixes

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,18 +1,2 @@
-set env vars
-set -o allexport; source .env; set +o allexport;
-
-mkdir -p ./config;
-mkdir -p ./src;
-mkdir -p ./package;
-mkdir -p ./yarn;
-mkdir -p ./.env;
-mkdir -p ./public/uploads;
-mkdir -p ./strapi-data;
-
-chmod 777 ./config
-chmod 777 ./src
-chmod 777 ./package
-chmod 777 ./yarn
-chmod 777 ./.env
-chmod 777 ./public/uploads
-chmod 777 ./strapi-data
+#set env vars
+#set -o allexport; source .env; set +o allexport;
