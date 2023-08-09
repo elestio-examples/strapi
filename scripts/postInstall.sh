c810873b8029b5a106ff4411504ba9c30c189ac5
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-09 20:45:20 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,7 +3,7 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+sleep 60s;
 
 target=$(docker-compose port strapi 1337)
 
