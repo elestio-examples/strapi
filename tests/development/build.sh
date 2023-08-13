6a1deac9bbcc20fc3806b3326a3881dfe8bedaa6
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-13 09:35:34 +0300

    fix

--- tests/development/build.sh
+++ tests/development/build.sh
@@ -1,3 +1,4 @@
 yes | npx create-strapi-app@latest my-project --dbforce --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=strapi --dbusername=postgres --dbpassword=postgres
 
-docker buildx build . --output type=docker,name=elestio4test/strapi-development:latest | docker load
+
+cd ./my-project && docker buildx build . --output type=docker,name=elestio4test/strapi-development:latest | docker load
--- tests/production/build.sh
+++ tests/production/build.sh
@@ -1,3 +1,3 @@
 yes | npx create-strapi-app@latest my-project --dbforce --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=strapi --dbusername=postgres --dbpassword=postgres
 
-docker buildx build . --output type=docker,name=elestio4test/strapi-production:latest | docker load
+cd ./my-project && docker buildx build . --output type=docker,name=elestio4test/strapi-production:latest | docker load
