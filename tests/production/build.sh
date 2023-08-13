52beb1224d6f2dd489dade0fbcfb614edad7708d
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-13 14:43:31 +0300

    fix

--- tests/production/build.sh
+++ tests/production/build.sh
@@ -5,15 +5,12 @@ cp ./Dockerfile ./app/Dockerfile
 cp ./.env ./app/.env
 cp ./entrypoint.sh ./app/entrypoint.sh
 
-sleep 10s;
-
 pwd=$(pwd)
 IFS="/"
 read -ra parts <<< "$pwd"
 folderName="${parts[-1]}"
-echo $folderName
-sed -i 's~PATH_TO_CHANGE~/opt/app/oss/temp/'"${folderName}"'~g' ./docker-compose.yml
+
+sed -i 's~PATH_TO_CHANGE~/opt/app/oss/temp/'"${folderName}"'~g' /opt/app/oss/temp/${folderName}/docker-compose.yml
 
 
-
-cd ./app && docker buildx build . --output type=docker,name=elestio4test/strapi-production:latest | docker load
+cd ./app && docker buildx build . --output type=docker,name=elestio4test/strapi-development:latest | docker load
