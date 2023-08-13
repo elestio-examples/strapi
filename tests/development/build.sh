3455e8f6c931bc58288af735dc62a39fb915464b
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-13 14:58:49 +0300

    fix

--- tests/development/build.sh
+++ tests/development/build.sh
@@ -1,3 +1,14 @@
+pwd=$(pwd)
+IFS="/"
+read -ra parts <<< "$pwd"
+folderName="${parts[-1]}"
+
+cat << EOT >> /opt/app/oss/temp/$folderName/.env
+
+folderName=/opt/app/oss/temp/$folderName
+EOT
+
+
 yes | npx create-strapi-app@latest app --dbforce --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=strapi --dbusername=postgres --dbpassword=postgres
 
 cp ./.dockerignore ./app/.dockerignore
@@ -5,15 +16,6 @@ cp ./Dockerfile ./app/Dockerfile
 cp ./.env ./app/.env
 cp ./entrypoint.sh ./app/entrypoint.sh
 
-pwd=$(pwd)
-IFS="/"
-read -ra parts <<< "$pwd"
-folderName="${parts[-1]}"
-
-cat << EOT >> ./.env
-
-folderName=$folderName
-EOT
 
 
 cd ./app && docker buildx build . --output type=docker,name=elestio4test/strapi-development:latest | docker load
