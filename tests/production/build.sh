968373b5eb6bc30d71a4b7dbcaf0d056539588d3
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-12-04 10:54:06 +0200

    fix

--- tests/production/build.sh
+++ tests/production/build.sh
@@ -4,5 +4,7 @@ cp ./.dockerignore ./app/.dockerignore
 cp ./Dockerfile ./app/Dockerfile
 cp ./.env ./app/.env
 cp ./entrypoint.sh ./app/entrypoint.sh
+chmod +x ./app/entrypoint.sh
+chmod +x ./entrypoint.sh
 
 cd ./app && touch yarn.lock && yarn install && docker buildx build . --output type=docker,name=elestio4test/strapi-production:latest | docker load && cd ..
