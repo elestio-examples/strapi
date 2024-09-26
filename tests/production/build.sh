61017150ad2bbe11eadb65113e12e6ce656a6f18
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-09-26 09:51:06 +0300

    fix

--- tests/production/build.sh
+++ tests/production/build.sh
@@ -1,4 +1,4 @@
-npx create-strapi-app@latest app --skip-cloud --js --install --no-git-init --example --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=strapi --dbusername=postgres --dbpassword=postgres
+yes | npx create-strapi-app@latest app --skip-cloud --js --install --no-git-init --example --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=strapi --dbusername=postgres --dbpassword=postgres
 
 cp ./.dockerignore ./app/.dockerignore
 cp ./Dockerfile ./app/Dockerfile
